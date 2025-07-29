c84cf8080dd497d2c8391de054ff5481636864a5
Mozzie <donytiara@gmail.com>
2025-07-30 00:33:53 +0700

    Puyeng

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,8 +1,14 @@
 return {
-    {
-      "neovim/nvim-lspconfig",
-      config = function()
-        require("lspconfig").lua_ls.setup {}
-      end,
-    }
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = { 'saghen/blink.cmp' },
+
+    -- example calling setup directly for each LSP
+    config = function()
+      local capabilities = require('blink.cmp').get_lsp_capabilities()
+      local lspconfig = require('lspconfig')
+
+      lspconfig['lua_ls'].setup({ capabilities = capabilities })
+    end
   }
+}

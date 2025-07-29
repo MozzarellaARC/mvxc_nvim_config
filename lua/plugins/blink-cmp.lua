1ab1b1ff3211d110708c66f3c0232cd684ade1d4
Mozzie <donytiara@gmail.com>
2025-07-30 06:57:14 +0700

    autocomplete

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -10,8 +10,6 @@ return {
   -- If you use nix, you can build from source using latest nightly rust with:
   -- build = 'nix run .#build-plugin',
 
-  ---@module 'blink.cmp'
-  ---@type blink.cmp.Config
   opts = {
     -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
     -- 'super-tab' for mappings similar to vscode (tab to accept)
@@ -33,8 +31,8 @@ return {
       nerd_font_variant = 'mono'
     },
 
-    -- (Default) Only show the documentation popup when manually triggered
-    completion = { documentation = { auto_show = false } },
+    -- Show documentation popup automatically
+    completion = { documentation = { auto_show = true, auto_show_delay_ms = 100 } },
 
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,14 +1,69 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    dependencies = { 'saghen/blink.cmp' },
+    dependencies = { 
+      'saghen/blink.cmp',
+      'williamboman/mason.nvim',
+      'williamboman/mason-lspconfig.nvim',
+     },
 
-    -- example calling setup directly for each LSP
     config = function()
       local capabilities = require('blink.cmp').get_lsp_capabilities()
-      local lspconfig = require('lspconfig')
+      
+      -- Common on_attach function for all LSP servers
+      local on_attach = function(client, bufnr)
+        -- Enable completion triggered by <c-x><c-o>
+        vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-      lspconfig['lua_ls'].setup({ capabilities = capabilities })
+        -- Mappings
+        local bufopts = { noremap=true, silent=true, buffer=bufnr }
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+        vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+        vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+        vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+      end
+
+      -- Setup language servers
+      local lspconfig = require('lspconfig')
+      
+      -- Lua Language Server
+      lspconfig.lua_ls.setup {
+        capabilities = capabilities,
+        on_attach = on_attach,
+        settings = {
+          Lua = {
+            runtime = {
+              version = 'LuaJIT',
+            },
+            diagnostics = {
+              globals = {'vim'},
+            },
+            workspace = {
+              library = vim.api.nvim_get_runtime_file("", true),
+            },
+            telemetry = {
+              enable = false,
+            },
+          },
+        },
+      }
+
+      -- Add more language servers as needed
+      -- Example for Python:
+      -- lspconfig.pyright.setup {
+      --   capabilities = capabilities,
+      --   on_attach = on_attach,
+      -- }
+      
+      -- Example for TypeScript/JavaScript:
+      -- lspconfig.ts_ls.setup {
+      --   capabilities = capabilities,
+      --   on_attach = on_attach,
+      -- }
     end
   }
 }

63dffb2a961baf6264df49ed5f83cc7e5c11113d
Mozzie <donytiara@gmail.com>
2025-07-29 19:36:01 +0700

    mini pairs and whatever

--- init.lua
+++ init.lua
@@ -1,2 +1,3 @@
 require("config.lazy")
 require("oil").setup()
+require("mason").setup()
---
+++ lua/plugins/mason.lua
@@ -0,0 +1,16 @@
+return{
+    "mason-org/mason.nvim",
+    opts = {},
+    config = function()
+        require("mason").setup({
+            ui = {
+                border = "rounded",
+                icons = {
+                    package_installed = "✓",
+                    package_pending = "➜",
+                    package_uninstalled = "✗"
+                }
+            }
+        })
+    end
+}

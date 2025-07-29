5505f7bced9e36572a5e75ec11bc50ad3eb984a8
Mozzie <donytiara@gmail.com>
2025-07-29 16:45:50 +0700

    test

--- lua/plugins/yazi.lua
+++ lua/plugins/yazi.lua
@@ -22,11 +22,5 @@ return {
             desc = "Resume the last yazi session",
         },
     },
-    opts = {
-        -- if you want to open yazi instead of netrw, see below for more info
-        open_for_directories = false,
-        keymaps = {
-            show_help = '<f1>',
-        },
-    },
+    opts = {}
 }

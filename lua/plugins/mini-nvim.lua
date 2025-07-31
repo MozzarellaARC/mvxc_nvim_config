e926f2dfcfdb36c12c64c59bc47c21dd3ef4e4b1
Mozzie <donytiara@gmail.com>
2025-07-31 17:36:52 +0700

    bubububububu

--- lua/plugins/mini-nvim.lua
+++ lua/plugins/mini-nvim.lua
@@ -2,5 +2,9 @@ return {
 	'echasnovski/mini.nvim', version = '*',
 	config = function ()
 		require('mini.pairs').setup()
+		require('mini.misc').setup()
+		
+		-- Auto root to the directory of the current buffer
+		require('mini.misc').setup_auto_root()
 	end
 }

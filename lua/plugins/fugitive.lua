a3eea70dc0844f3d1f768f6b8a0e8d6032db10fc
Mozzie <donytiara@gmail.com>
2025-07-31 14:44:46 +0700

    ttttt

---
+++ lua/plugins/fugitive.lua
@@ -0,0 +1,17 @@
+return {
+  -- Git integration for Neovim with GitHub support
+  {
+    "tpope/vim-fugitive",
+    -- cmd = { "Git", "G", "Gdiffsplit", "Gread", "Gwrite", "Ggrep", "GMove", "GDelete", "GBrowse", "GRemove", "GRename", "Glgrep", "Gedit" },
+    ft = { "fugitive" },
+    keys = {
+      { "<leader>gd", ":vert Gdiffsplit<CR>", desc = "Git diff split" },
+      { "<leader>gs", "<Cmd>Git<CR>", desc = "Git Status" },
+    },
+    dependencies = {
+      "tpope/vim-rhubarb", -- GitHub integration for fugitive
+    },
+    config = function()
+    end,
+  },
+}

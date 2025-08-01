63a0a31c90237211c82d11e8ebd78d5ddc167eed
Mozzie <donytiara@gmail.com>
2025-08-02 04:45:08 +0700

    formatter test

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -5,14 +5,12 @@ local map = vim.keymap.set
 -- vim.opt.timeoutlen = 300
 
 -- Disable default keybindings
-map({ 'n', 'i', 'v' }, '<C-r>', '<Nop>')
-map({ 'n', 'v' }, 'h', '<Nop>')
-map({ 'n', 'v' }, 'j', '<Nop>')
-map({ 'n', 'v' }, 'k', '<Nop>')
-map({ 'n', 'v' }, 'l', '<Nop>')
-map({ 'n', 'v' }, 'Q', '<Nop>')
-
-
+map({ "n", "i", "v" }, "<C-r>", "<Nop>")
+map({ "n", "v" }, "h", "<Nop>")
+map({ "n", "v" }, "j", "<Nop>")
+map({ "n", "v" }, "k", "<Nop>")
+map({ "n", "v" }, "l", "<Nop>")
+map({ "n", "v" }, "Q", "<Nop>")
 
 -- IONS, FUNCTIOS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUU
 -- FUNCTIONS, FUNCTIOS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS
@@ -63,16 +61,16 @@ map({ 'n', 'v' }, 'Q', '<Nop>')
 -- FUNCTIONS, FUNCTIOS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS
 
 ---General keybindings
-map('n', 'z', 'u')               -- undo with z
-map('n', '<C-S-z>', '<C-r>')     -- Redo with Shift+z
+map("n", "z", "u") -- undo with z
+map("n", "<C-S-z>", "<C-r>") -- Redo with Shift+z
 
-map('n', '<C-s>', ':w<CR>')      -- Save with Ctrl+s
-map('i', '<C-s>', '<Esc>:w<CR>') -- Save with Ctrl+s
-map('v', '<C-s>', '<Esc>:w<CR>') -- Save with Ctrl+s
+map("n", "<C-s>", ":w<CR>") -- Save with Ctrl+s
+map("i", "<C-s>", "<Esc>:w<CR>") -- Save with Ctrl+s
+map("v", "<C-s>", "<Esc>:w<CR>") -- Save with Ctrl+s
 -- map('n', '<C-S>s', ':wa<CR>') -- Save all buffers with Ctrl+Alt+s If this is turned on, it will gives delays to the Ctrl+s keybinding
 
-map('n', '<F4>', ':qa!<CR>') -- Exit Neovim
-map('v', '<C-c>', 'y')       -- Copy selection with Ctrl+c
+map("n", "<F4>", ":qa!<CR>") -- Exit Neovim
+map("v", "<C-c>", "y") -- Copy selection with Ctrl+c
 
 --- Fugitive keybindings
 -- map("n", "<leader>gs", ":Git<CR>")
@@ -85,40 +83,43 @@ map('v', '<C-c>', 'y')       -- Copy selection with Ctrl+c
 -- map("v", "<leader>gh", ":GBrowse<CR>")
 
 -- Buffer navigation keybindings
-map('n', '<Tab>', ':wincmd w<CR>')
-map('n', '<C-S-Left>', '<Cmd>BufferPrevious<CR>') -- Go to previous buffer
-map('n', '<C-S-Right>', '<Cmd>BufferNext<CR>')    -- Go to next buffer
+map("n", "<Tab>", ":wincmd w<CR>")
+map("n", "<C-S-Left>", "<Cmd>BufferPrevious<CR>") -- Go to previous buffer
+map("n", "<C-S-Right>", "<Cmd>BufferNext<CR>") -- Go to next buffer
 
 -- Indentation keybindings
-map('n', '<C-,>', '<<')
-map('n', '<C-.>', '>>')
+map("n", "<C-,>", "<<")
+map("n", "<C-.>", ">>")
 
-map('i', '<C-,>', '<C-d>')
-map('i', '<C-.>', '<C-t>')
+map("i", "<C-,>", "<C-d>")
+map("i", "<C-.>", "<C-t>")
 
-map('v', '<C-,>', '<gv')
-map('v', '<C-.>', '>gv')
+map("v", "<C-,>", "<gv")
+map("v", "<C-.>", ">gv")
 
 -- Comment-out keybindings
-map('n', '<C-/>', '<Cmd>normal gcc<CR>', { silent = true })
-map('v', '<C-/>', '<Cmd>normal gcgv<CR>', { silent = true })
-map('i', '<C-/>', '<Esc><Cmd>normal gcc<CR>i', { silent = true })
+map("n", "<C-/>", "<Cmd>normal gcc<CR>", { silent = true })
+map("v", "<C-/>", "<Cmd>normal gcgv<CR>", { silent = true })
+map("i", "<C-/>", "<Esc><Cmd>normal gcc<CR>i", { silent = true })
 
 --- Yazi keybindings
-map('n', '<space><space>', '<Cmd>Yazi<CR>') -- Open yazi at current file
-map('v', '<space><space>', '<Cmd>Yazi<CR>') -- Open yazi at current file
-map('n', '<space>cw', '<Cmd>Yazi cwd<CR>')  -- Open yazi in working directory
-
+map("n", "<space><space>", "<Cmd>Yazi<CR>") -- Open yazi at current file
+map("v", "<space><space>", "<Cmd>Yazi<CR>") -- Open yazi at current file
+map("n", "<space>cw", "<Cmd>Yazi cwd<CR>") -- Open yazi in working directory
 
 -- Directory tree keybindings
-map('n', '<F1>', '<Cmd>Neotree toggle<CR>')-- Open yazi in current file
-map('v', '<F1>', '<Cmd>Neotree toggle<CR>')-- Open yazi in current file
+map("n", "<F1>", "<Cmd>Neotree toggle<CR>") -- Open yazi in current file
+map("v", "<F1>", "<Cmd>Neotree toggle<CR>") -- Open yazi in current file
 
-map('n', '<F2>', function() _G.undotree_toggle() end, { desc = "Toggle Undotree" }) -- Toggle undotree
-map('v', '<F2>', function() _G.undotree_toggle() end, { desc = "Toggle Undotree" }) -- Toggle undotree
+map("n", "<F2>", function()
+	_G.undotree_toggle()
+end, { desc = "Toggle Undotree" }) -- Toggle undotree
+map("v", "<F2>", function()
+	_G.undotree_toggle()
+end, { desc = "Toggle Undotree" }) -- Toggle undotree
 
-map('n', '<F3>', '<cmd>Trouble diagnostics toggle<cr>')
-map('v', '<F3>', '<cmd>Trouble diagnostics toggle<cr>')
+map("n", "<F3>", "<cmd>Trouble diagnostics toggle<cr>")
+map("v", "<F3>", "<cmd>Trouble diagnostics toggle<cr>")
 
 -- IONS, FUNCTIOS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUU
 -- FUNCTIONS, FUNCTIOS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS
@@ -128,41 +129,43 @@ map('v', '<F3>', '<cmd>Trouble diagnostics toggle<cr>')
 -- FUNCTIONS, FUNCTIOS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS
 
 -- Override the entire C-w prefix with conditional behavior
-map('n', '<C-w>', function()
+map("n", "<C-w>", function()
 	-- Get the number of windows in current tab
-	local win_count = vim.fn.winnr('$')
+	local win_count = vim.fn.winnr("$")
 	-- Also check if current window is the last "normal" window
 	-- (excluding special windows like quickfix, help, etc.)
 	local current_win = vim.api.nvim_get_current_win()
 	local buf = vim.api.nvim_win_get_buf(current_win)
 	local buftype = vim.bo[buf].buftype
-	if win_count > 1 and buftype == '' then
+	if win_count > 1 and buftype == "" then
 		-- Multiple windows and current is a normal buffer: close current window
-		local ok = pcall(function() vim.cmd('close') end)
+		local ok = pcall(function()
+			vim.cmd("close")
+		end)
 		if not ok then
 			-- If close fails, fallback to deleting buffer
-			vim.cmd('bd')
+			vim.cmd("bd")
 		end
 	else
 		-- Single window or special buffer type: delete buffer
-		vim.cmd('bd')
+		vim.cmd("bd")
 	end
 end, { noremap = true, nowait = true })
 
 -- Toggle function for Diffview
 local function toggle_diffview()
-	local view = require('diffview.lib').get_current_view()
+	local view = require("diffview.lib").get_current_view()
 	if view then
 		-- If diffview is open, close it
-		vim.cmd('DiffviewClose')
+		vim.cmd("DiffviewClose")
 	else
 		-- If diffview is closed, open it
-		vim.cmd('DiffviewOpen')
+		vim.cmd("DiffviewOpen")
 	end
 end
 
 -- Set up the keymap
-vim.keymap.set('n', '<F5>', toggle_diffview, { desc = 'Toggle Diffview' })
+vim.keymap.set("n", "<F5>", toggle_diffview, { desc = "Toggle Diffview" })
 
 -- IONS, FUNCTIOS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUU
 -- FUNCTIONS, FUNCTIOS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS, FUNCTIONS
--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,86 +1,88 @@
 return {
-  'stevearc/conform.nvim',
-  event = { "BufWritePre" },
-  cmd = { "ConformInfo" },
-  keys = {
-    {
-      -- Customize or remove this keymap to your liking
-      "<leader>f",
-      function()
-        require("conform").format({ async = true, lsp_fallback = true })
-      end,
-      mode = "",
-      desc = "Format buffer",
-    },
-  },
-  -- Everything in opts will be passed to setup()
-  opts = {
-    -- Define your formatters
-    formatters_by_ft = {
-      lua = { "stylua" },
-      python = { "isort", "black" },
-      javascript = { { "prettierd", "prettier" } },
-      typescript = { { "prettierd", "prettier" } },
-      javascriptreact = { { "prettierd", "prettier" } },
-      typescriptreact = { { "prettierd", "prettier" } },
-      vue = { { "prettierd", "prettier" } },
-      css = { { "prettierd", "prettier" } },
-      scss = { { "prettierd", "prettier" } },
-      less = { { "prettierd", "prettier" } },
-      html = { { "prettierd", "prettier" } },
-      json = { { "prettierd", "prettier" } },
-      jsonc = { { "prettierd", "prettier" } },
-      yaml = { { "prettierd", "prettier" } },
-      markdown = { { "prettierd", "prettier" } },
-      graphql = { { "prettierd", "prettier" } },
-      handlebars = { "prettier" },
-      go = { "goimports", "gofmt" },
-      rust = { "rustfmt" },
-      c = { "clang_format" },
-      cpp = { "clang_format" },
-      java = { "google-java-format" },
-      sh = { "shfmt" },
-      bash = { "shfmt" },
-      zsh = { "shfmt" },
-      fish = { "fish_indent" },
-      xml = { "xmlformat" },
-      sql = { "sql_formatter" },
-      -- Use the "*" filetype to run formatters on all filetypes.
-      -- ["*"] = { "codespell" },
-      -- Use the "_" filetype to run formatters on filetypes that don't
-      -- have other formatters configured.
-      -- ["_"] = { "trim_whitespace" },
-    },
-    -- Set up format-on-save
-    format_on_save = {
-      -- These options will be passed to conform.format()
-      timeout_ms = 500,
-      lsp_fallback = true,
-    },
-    -- Set up format after save for async formatting
-    -- format_after_save = {
-    --   lsp_fallback = true,
-    -- },
-    -- Customize formatters
-    formatters = {
-      shfmt = {
-        prepend_args = { "-i", "2" },
-      },
-      -- # Example of using dprint only when a dprint.json file is present
-      -- dprint = {
-      --   condition = function(ctx)
-      --     return vim.fs.find({ "dprint.json" }, { path = ctx.filename, upward = true })[1]
-      --   end,
-      -- },
-      --
-      -- # Example of using shfmt with extra args
-      -- shfmt = {
-      --   prepend_args = { "-i", "2", "-ci" },
-      -- },
-    },
-  },
-  init = function()
-    -- If you want the formatexpr, here is the place to set it
-    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-  end,
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	keys = {
+		{
+			-- Customize or remove this keymap to your liking
+			"<leader>f",
+			function()
+				require("conform").format({ async = true, lsp_fallback = true })
+			end,
+			mode = "",
+			desc = "Format buffer",
+		},
+	},
+	-- Everything in opts will be passed to setup()
+	opts = {
+		-- Define your formatters
+		formatters_by_ft = {
+			zig = { "zigfmt" },
+			lua = { "stylua" },
+			python = { "isort", "black" },
+			javascript = { { "prettierd", "prettier" } },
+			typescript = { { "prettierd", "prettier" } },
+			javascriptreact = { { "prettierd", "prettier" } },
+			typescriptreact = { { "prettierd", "prettier" } },
+			vue = { { "prettierd", "prettier" } },
+			css = { { "prettierd", "prettier" } },
+			scss = { { "prettierd", "prettier" } },
+			less = { { "prettierd", "prettier" } },
+			html = { { "prettierd", "prettier" } },
+			json = { { "prettierd", "prettier" } },
+			jsonc = { { "prettierd", "prettier" } },
+			yaml = { { "prettierd", "prettier" } },
+			markdown = { { "prettierd", "prettier" } },
+			graphql = { { "prettierd", "prettier" } },
+			handlebars = { "prettier" },
+			go = { "goimports", "gofmt" },
+			rust = { "rustfmt" },
+			c = { "clang_format" },
+			cpp = { "clang_format" },
+			java = { "google-java-format" },
+			sh = { "shfmt" },
+			bash = { "shfmt" },
+			zsh = { "shfmt" },
+			fish = { "fish_indent" },
+			xml = { "xmlformat" },
+			sql = { "sql_formatter" },
+			-- Use the "*" filetype to run formatters on all filetypes.
+			-- ["*"] = { "codespell" },
+			-- Use the "_" filetype to run formatters on filetypes that don't
+			-- have other formatters configured.
+			-- ["_"] = { "trim_whitespace" },
+		},
+		-- Set up format-on-save
+		format_on_save = {
+			-- These options will be passed to conform.format()
+			timeout_ms = 500,
+			lsp_fallback = true,
+		},
+		-- Set up format after save for async formatting
+		-- format_after_save = {
+		--   lsp_fallback = true,
+		-- },
+		-- Customize formatters
+		formatters = {
+			shfmt = {
+				prepend_args = { "-i", "2" },
+			},
+			-- # Example of using dprint only when a dprint.json file is present
+			-- dprint = {
+			--   condition = function(ctx)
+			--     return vim.fs.find({ "dprint.json" }, { path = ctx.filename, upward = true })[1]
+			--   end,
+			-- },
+			--
+			-- # Example of using shfmt with extra args
+			-- shfmt = {
+			--   prepend_args = { "-i", "2", "-ci" },
+			-- },
+		},
+	},
+	init = function()
+		-- If you want the formatexpr, here is the place to set it
+		vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+	end,
 }
+

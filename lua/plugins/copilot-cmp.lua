a2a77bf391e400e7ca7b01ed0c82ed9199e38c5f
Mozzie <donytiara@gmail.com>
2025-08-02 06:00:30 +0700

    copilot official

--- lua/plugins/copilot-cmp.lua
+++ lua/plugins/copilot-cmp.lua
@@ -1,18 +1,18 @@
 return {
-  "zbirenbaum/copilot-cmp",
-  dependencies = {
-    "zbirenbaum/copilot.lua",
-  },
-  config = function()
-    require("copilot_cmp").setup({
-      -- Method for getting completions
-      method = "getCompletionsCycling",
-      -- Format the completion item
-      formatters = {
-        label = require("copilot_cmp.format").format_label_text,
-        insert_text = require("copilot_cmp.format").format_insert_text,
-        preview = require("copilot_cmp.format").deindent,
-      },
-    })
-  end,
+	"zbirenbaum/copilot-cmp",
+	dependencies = {
+		"zbirenbaum/copilot.lua",
+	},
+	config = function()
+		require("copilot_cmp").setup({
+			-- Method for getting completions
+			method = "getCompletionsCycling",
+			-- Format the completion item
+			formatters = {
+				label = require("copilot_cmp.format").format_label_text,
+				insert_text = require("copilot_cmp.format").format_insert_text,
+				preview = require("copilot_cmp.format").deindent,
+			},
+		})
+	end,
 }
